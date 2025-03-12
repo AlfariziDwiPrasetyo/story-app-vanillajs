c2865b0322114b3f9ccab6ffdb0335ddac2fad77
Ryuugamine <yoerdanatan@gmail.com>
2025-03-13 02:32:45 +0700

    feat : add register page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,11 +2,12 @@ import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
 import LoginPage from '../pages/auth/login/login-page';
 import { checkAuthenticatedRoute, checkUnauthenticatedRouteOnly } from '../utils/auth';
+import RegisterPage from '../pages/auth/register/register-page';
 
 const routes = {
   '/': () => checkAuthenticatedRoute(new HomePage()),
   '/login': () => checkUnauthenticatedRouteOnly(new LoginPage()),
-  // '/register': () => checkUnauthenticatedRouteOnly(new RegisterPage()),
+  '/register': () => checkUnauthenticatedRouteOnly(new RegisterPage()),
   // '/about': new AboutPage(),
 };
 
